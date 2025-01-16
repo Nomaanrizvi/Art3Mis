89d8cf15e66ec4a22965ea7db32648223ef2922c
nomaan rizvi <nomanrizvi007@gmail.com>
2025-01-16 14:18:28 +0530

    fixed the ui for cropped image

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ _id, name, prompt, photo }) => {
   return (
     <div className='card rounded-xl group relative shadow-card hover:shadow-cardhover'>
       <img
-        className='w-full h-auto object-cover rounded-xl'
+        className='w-full h-full object-cover rounded-xl'
         src={photo}
         alt={prompt}
       />

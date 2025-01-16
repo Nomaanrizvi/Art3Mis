55eb2f47da8d42922c686ecb0c7c43bf77be7cf7
nomaan rizvi <nomanrizvi007@gmail.com>
2025-01-16 12:50:19 +0530

    made ui changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
-import {logo} from './assets'
+import { logo } from './assets'
 import Home from './pages/Home'
 import CreatePost from './pages/CreatePost'
 
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
       <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
         <Link to="/">
-          <img src={logo} alt="logo" className='w-28 object-contain'/>
+          <img src='\src\assets\logo_2.png' alt="logo" className='w-40 object-contain'/>
         </Link>
 
         <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
@@ -18,7 +18,7 @@ function App() {
         </Link>
       </header>
 
-      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh - 73px)]'>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f0f3ff] min-h-[calc(100vh - 73px)]'>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/create-post' element={<CreatePost/>}/>

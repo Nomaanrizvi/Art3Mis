79a97056ee3397251332c2c086ddb2ba8f86e4d8
nomaan rizvi <nomanrizvi007@gmail.com>
2025-01-18 09:04:18 +0530

    logo issue fix 2.0

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { logo, preview } from '../assets'
+import { preview } from '../assets'
 import { getRandomPrompt } from '../utils'
 import FormField from '../Components/FormField'
 import Loader from '../Components/Loader'
